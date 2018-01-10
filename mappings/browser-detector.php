ca875e01f30282ade7d51834a1fd429c93a1b7b8
Thomas Müller <thomas.mueller@geld.de>
2018-01-10 16:39:44 +0100

    implement Symfony/Finder, add Mappings for BrowserDetector

---
+++ mappings/browser-detector.php
@@ -0,0 +1,43 @@
+<?php
+
+return [
+    'browser' => [
+        'name' => [
+            'internetexplorer'             => 'ie',
+            'chromeforios'                 => 'chrome',
+            'edgemobile'                   => 'edge',
+        ],
+        'version' => [
+            '0.0' => '',
+        ]
+    ],
+    'platform' => [
+        'name' => [
+            'windowsnt61'    => 'win7',
+            'windowsnt62'    => 'win8',
+            'windowsnt100'   => 'win10',
+            'windowsphoneos' => 'winphone',
+        ],
+        'version' => [
+            '0.0'  => '',
+            '10.0' => '10',
+        ]
+    ],
+    'device' => [
+        'name' => [
+            'galaxycoreprimeltemetropcs' => 'samsungsmg360t1',
+        ],
+        'brand' => [
+            'appleinc'          => 'apple',
+            'blackberrylimited' => 'researchinmotionlimited',
+            'googleinc'         => 'google',
+            'nttdocomo'         => 'docomo',
+            'teslamotors'       => 'tesla',
+            'unknown'           => '',
+            'wetabgmbh'         => 'wetab',
+        ],
+        'type' => [
+            'unknown' => '',
+        ]
+    ]
+];
